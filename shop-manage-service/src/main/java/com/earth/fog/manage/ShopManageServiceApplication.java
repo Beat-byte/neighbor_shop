1320d4fd32ec6f5492ad3c6bc8acf57457b5d483
若烟 <497400422@qq.com>
2020-04-18 18:45:50 +0800

    框架搭建

---
+++ shop-manage-service/src/main/java/com/earth/fog/manage/ShopManageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.earth.fog.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopManageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopManageServiceApplication.class, args);
+    }
+
+}
---
+++ shop-manage-web/src/test/java/com/earth/fog/manage/ShopManageWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earth.fog.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopManageWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
